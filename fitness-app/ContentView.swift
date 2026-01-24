43287ea134cb16bc5881c3abee009cf381e755ef
domkish <dominickish@gmail.com>
2026-01-24 00:59:46 -0500

    Initial Commit

---
+++ fitness-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  fitness-app
+//
+//  Created by Dominic Kish on 1/24/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ fitness-app/fitness_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  fitness_appApp.swift
+//  fitness-app
+//
+//  Created by Dominic Kish on 1/24/26.
+//
+
+import SwiftUI
+
+@main
+struct fitness_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
